1dcd879d35e3506f1576092d607e5c085d76be75
Matus <matusino.sykora@gmail.com>
2020-09-08 22:02:13 +0200

    basic CRUD for UserRepository

--- src/main/java/com/matus/expenzor/service/UserService.java
+++ src/main/java/com/matus/expenzor/service/UserService.java
@@ -1,4 +1,22 @@
 package com.matus.expenzor.service;
 
+import com.matus.expenzor.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
 public interface UserService {
+
+    User saveUser(User user);
+
+    User findUserById(Long id);
+
+    List<User> getAllUsers();
+
+    User updateExistingUser(User user);
+
+    void deleteUserById(Long id);
+
 }
