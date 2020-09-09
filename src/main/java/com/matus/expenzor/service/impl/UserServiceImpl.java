649e200250e89ceef69272c10bffe1624bad9758
Matus <matusino.sykora@gmail.com>
2020-09-09 08:25:09 +0200

    added userService implementation of methods

--- src/main/java/com/matus/expenzor/service/impl/UserServiceImpl.java
+++ src/main/java/com/matus/expenzor/service/impl/UserServiceImpl.java
@@ -1,4 +1,45 @@
 package com.matus.expenzor.service.impl;
 
-public class UserServiceImpl {
+import com.matus.expenzor.model.User;
+import com.matus.expenzor.repository.UserRepository;
+import com.matus.expenzor.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<>();
+        userRepository.findAll().forEach(userList::add);
+        return userList;
+    }
+
+    @Override
+    public User updateExistingUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
 }
