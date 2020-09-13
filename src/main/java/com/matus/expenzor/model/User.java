4d87e28006aaae3afbeb8a870133d05d4127cf0b
Matus <matusino.sykora@gmail.com>
2020-09-13 18:12:42 +0200

    refactor User class, removed lastName

--- src/main/java/com/matus/expenzor/model/User.java
+++ src/main/java/com/matus/expenzor/model/User.java
@@ -16,10 +16,7 @@ public class User {
     private Long id;
 
     @NotEmpty(message = "Please provide your first name")
-    private String firstName;
-
-    @NotEmpty(message = "Please provide your last name")
-    private String lastName;
+    private String userName;
 
     @NotEmpty(message = "Please provide valid email address")
     private String emailAddress;
@@ -37,10 +34,9 @@ public class User {
     public User() {
     }
 
-    public User(Long id, String firstName, String lastName, String emailAddress, String password) {
+    public User(Long id, String userName, String emailAddress, String password) {
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.userName = userName;
         this.emailAddress = emailAddress;
         this.password = password;
     }
@@ -53,20 +49,12 @@ public class User {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setuUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getEmailAddress() {
@@ -107,23 +95,21 @@ public class User {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return Objects.equals(id, user.id) &&
-                Objects.equals(firstName, user.firstName) &&
-                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(userName, user.userName) &&
                 Objects.equals(emailAddress, user.emailAddress) &&
                 Objects.equals(password, user.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, emailAddress, password);
+        return Objects.hash(id, userName, emailAddress, password);
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
+                ", firstName='" + userName + '\'' +
                 ", emailAddress='" + emailAddress + '\'' +
                 ", password='" + password + '\'' +
                 '}';
