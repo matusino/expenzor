d78c173f42fb4eb409798b233437e672133e6e4c
Matus <matusino.sykora@gmail.com>
2020-09-13 17:16:01 +0200

    security configuration

--- src/main/java/com/matus/expenzor/config/SecurityConfig.java
+++ src/main/java/com/matus/expenzor/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.matus.expenzor.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -13,7 +18,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/auth")
                 .permitAll()
-                .anyRequest()
-                .authenticated();
+                ;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
