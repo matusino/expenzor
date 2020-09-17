384934f1fbdc216c78528a7927728a29a2e08e28
Matus <matusino.sykora@gmail.com>
2020-09-17 12:10:38 +0200

    swagger added - simple REST API doc.

--- src/main/java/com/matus/expenzor/ExpenzorApplication.java
+++ src/main/java/com/matus/expenzor/ExpenzorApplication.java
@@ -1,20 +1,16 @@
 package com.matus.expenzor;
 
+import com.matus.expenzor.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class ExpenzorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenzorApplication.class, args);
 	}
 
-//	@Bean
-//	PasswordEncoder passwordEncoder(){
-//		return new BCryptPasswordEncoder();
-//	}
 }
