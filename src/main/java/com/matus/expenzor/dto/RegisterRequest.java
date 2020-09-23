2806875945ea75ae98f8b348faf7d3a77b8ab748
Matus <matusino.sykora@gmail.com>
2020-09-23 22:55:31 +0200

    username typo fixed

--- src/main/java/com/matus/expenzor/dto/RegisterRequest.java
+++ src/main/java/com/matus/expenzor/dto/RegisterRequest.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
-    private String userName;
+    private String username;
     private String email;
     private String password;
 }
