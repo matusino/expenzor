bb0f880512d9e24a5d188c26650e2255f50e9afe
Matus <matusino.sykora@gmail.com>
2020-09-23 22:53:50 +0200

    cors configuration

--- src/main/java/com/matus/expenzor/ExpenzorApplication.java
+++ src/main/java/com/matus/expenzor/ExpenzorApplication.java
@@ -3,11 +3,27 @@ package com.matus.expenzor;
 import com.matus.expenzor.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Import(SwaggerConfig.class)
+@CrossOrigin(origins = "*")
 public class ExpenzorApplication {
+
+	@Bean
+	public WebMvcConfigurer corsCOnfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedHeaders("*").allowedOrigins("*").allowedMethods("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenzorApplication.class, args);
--- src/main/java/com/matus/expenzor/config/SecurityConfig.java
+++ src/main/java/com/matus/expenzor/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.matus.expenzor.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -30,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable();
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**")
@@ -40,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                         "/configuration/security",
                         "/swagger-ui.html",
                         "/webjars/**")
+                .permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
