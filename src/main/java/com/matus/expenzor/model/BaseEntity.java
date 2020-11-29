2f293435fb93f2ad4889e9c74927ca79adaf042c
Matus <matusino.sykora@gmail.com>
2020-11-29 21:06:54 +0100

    base entity implementation

---
+++ src/main/java/com/matus/expenzor/model/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.matus.expenzor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String city;
+    private String phoneNumber;
+    private int age;
+}
