58caed17402578d6d200213548e345b87510d160
Matus <matusino.sykora@gmail.com>
2020-12-25 21:45:40 +0100

    validation annotations added

--- src/main/java/com/matus/expenzor/model/BaseEntity.java
+++ src/main/java/com/matus/expenzor/model/BaseEntity.java
@@ -3,6 +3,7 @@ package com.matus.expenzor.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,10 +19,21 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Nullable
     private String firstName;
+
+    @Nullable
     private String lastName;
+
+    @Nullable
     private String gender;
+
+    @Nullable
     private String city;
+
+    @Nullable
     private String phoneNumber;
+
+    @Nullable
     private int age;
 }
