10083e71baa053f6fcbe2023c080fd83317cf704
Matus <matusino.sykora@gmail.com>
2020-12-26 13:13:57 +0100

    AuthenticationResponse renamed to RegisterResponse

--- src/main/java/com/matus/expenzor/dto/auth/RegisterResponse.java
+++ src/main/java/com/matus/expenzor/dto/auth/RegisterResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationResponse {
+public class RegisterResponse {
     private String authToken;
     private String username;
 }
