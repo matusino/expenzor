15b71608a8a6622f06617efdd8e99d9d9bc8fc9b
Matus <matusino.sykora@gmail.com>
2021-01-02 19:55:58 +0100

    UserDTO validators

--- src/main/java/com/matus/expenzor/dto/user/UserDTO.java
+++ src/main/java/com/matus/expenzor/dto/user/UserDTO.java
@@ -4,7 +4,10 @@ import com.matus.expenzor.dto.expense.ExpenseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +16,32 @@ import java.util.List;
 @NoArgsConstructor
 public class UserDTO {
 
+    @NotEmpty
     private Long id;
+
+    @Nullable
     private String firstName;
+
+    @Nullable
     private String lastName;
+
+    @Nullable
     private String gender;
+
+    @Nullable
     private String city;
+
+    @Nullable
     private String phoneNumber;
+
+    @Nullable
     private int age;
+
+    @NotEmpty
     private String userName;
+
+    @NotEmpty
+    @Email
     private String emailAddress;
 
     private List<ExpenseDto> expenses = new ArrayList<>();
